8d30d0b2677d29e99a4a68255ccb545728a05f32
ProfRa1 <raissavanessa781@hotmail.com>
2023-06-27 10:42:43 -0300

    first commit

---
+++ screens/Search.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+
+
+export default class SearchScreen extends React.Component{
+  render(){
+    return(
+      <View>
+      <Text> Tela de Pesquisa </Text>
+      </View>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
